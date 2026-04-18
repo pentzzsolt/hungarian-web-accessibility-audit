2e27809a6829dd962718187ec9fef81a82aa4743
Zsolt Pentz <3056515+pentzzsolt@users.noreply.github.com>
2026-04-18 23:50:13 +0200

    fix: filter by p99

--- scripts/generate_error_summary.mjs
+++ scripts/generate_error_summary.mjs
@@ -1,17 +1,19 @@
 import { readdir, readFile, writeFile } from 'fs/promises';
 import { join, resolve } from 'path';
 import { stringify } from 'csv-stringify/sync'
+import { getSampleP99 } from './utils/get_sample.mjs';
 
 const output_file_path = resolve('results/error_type_summary.csv'),
       audits_path = resolve('results/audits'),
       dateDirs = await readdir(audits_path, { withFileTypes: true }),
-      output = [];
+      output = [],
+      domains = await getSampleP99();
 
 for (const dirent of dateDirs) {
   if (!dirent.isDirectory()) continue;
   const date = dirent.name;
   const datePath = join(audits_path, date);
-  const files = await readdir(datePath);
+  const files = (await readdir(datePath)).filter(file => domains.includes(file.replace(/\.json$/, '')));
   const errors = [];
 
   for (const file of files) {
--- scripts/utils/get_sample.mjs
+++ scripts/utils/get_sample.mjs
@@ -10,3 +10,12 @@ export async function getSample() {
         });
   return sample_domains.map(domain => domain.Domain)
 }
+
+export async function getSampleP99(full = false) {
+  const sample_domains_file_path = resolve('data/processed/analytical_sample_p99.csv'),
+        sample_domains_file = await readFile(sample_domains_file_path, 'utf8'),
+        sample_domains = parse(sample_domains_file, {
+          columns: true
+        });
+  return sample_domains.map(domain => full ? domain : domain.Domain)
+}
